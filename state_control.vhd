7333840f1e999e8e108dd9f59603f17dd13e3e53
christopher-alba <calb200@aucklanduni.ac.nz>
2021-05-26 11:12:48 +1200

    Add files via upload

--- state_control.vhd
+++ state_control.vhd
@@ -4,26 +4,78 @@ USE  IEEE.STD_LOGIC_ARITH.all;
 USE  IEEE.STD_LOGIC_SIGNED.all;
 
 entity state_control is
-	port(b2, b1, b0, game_over: IN STD_LOGIC;			
+	port(clk, b2, b1, b0, game_over: IN STD_LOGIC;			
 			state : OUT STD_LOGIC_VECTOR(1 downto 0));
 end entity state_control;
 
 architecture a of state_control is
+-- FSM signals
+type state_type is (s_menu, s_regular, s_training, s_over);
+signal state_s, next_state : state_type := s_menu;
+
 begin
-	process(b2, b1, b0, game_over)
-	variable temp : STD_LOGIC_VECTOR(1 downto 0) := "00";
-	begin -- 00 mainmenu 01 game 10 training 11 death screen		
-		
-		if (b0 = '0') then -- go to training mode
-			temp := "10";
-		elsif (b1 = '0') then -- start level 1
-			temp := "01";
-		elsif (b2 = '0') then -- main menu
-			temp := "00";
-		elsif (rising_edge(game_over)) then
-			temp := "11";
+	--synchronously move to next state
+	sync_proc : process(clk, b2) 
+	begin
+		if (b2 = '0') then
+			state_s <= s_menu;
+		elsif (rising_edge(clk)) then
+			state_s <= next_state;
 		end if;
-		
-	state <= temp;
 	end process;
+	
+	--asynchronously decide next state based only on current state and inputs
+	next_states_fn: process(state_s, b1, b2, b0, game_over) 
+	begin
+		case(state_s) is
+			when s_menu =>
+				if (b0 = '0') then 					
+					next_state <= s_training;
+				elsif (b1 = '0') then				
+					next_state <= s_regular;
+				else 									
+					next_state <= s_menu;
+				end if;
+				
+			when s_training =>
+				if (game_over = '1') then 
+					next_state <= s_over;
+				elsif (b2 = '0') then 
+					next_state <= s_menu;	
+				else
+					next_state <= s_training;
+				end if;
+				
+			when s_regular =>
+				if (game_over = '1') then 
+					next_state <= s_over;
+				elsif (b2 = '0') then 
+					next_state <= s_menu;
+				else
+					next_state <= s_regular;
+				end if;
+				
+			when s_over =>
+				if (b2 = '0') then
+					next_state <= s_menu;
+				else 
+					next_state <= s_over;
+				end if;
+				
+		end case;
+	end process;
+	
+	outputs:process(state_s)
+		begin
+			case state_s is
+				when s_menu =>
+					state <= "00";
+				when s_training =>
+					state <= "10";
+				when s_regular =>
+					state <= "01";
+				when s_over =>
+					state <= "11";
+			end case;
+		end process;
 end a;
