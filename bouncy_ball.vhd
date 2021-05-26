fb5438e8f465c98f8946bcec8de5f090d05f2773
christopher-alba <calb200@aucklanduni.ac.nz>
2021-05-26 14:54:41 +1200

    Add files via upload

--- bouncy_ball.vhd
+++ bouncy_ball.vhd
@@ -9,7 +9,7 @@ ENTITY bouncy_ball IS
 		( clk, vert_sync, left_click, pause, reset	: IN std_logic;
         pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
 		  state : IN STD_LOGIC_VECTOR(1 downto 0);
-		  bird_on_signal: OUT std_logic);
+		  bird_on_signal, bird_dead: OUT std_logic);
 END bouncy_ball;
 
 architecture behavior of bouncy_ball is
@@ -24,6 +24,7 @@ BEGIN
 gravity <= CONV_STD_LOGIC_VECTOR(3,10) when pause = '0' else CONV_STD_LOGIC_VECTOR(0,10);
 size <= CONV_STD_LOGIC_VECTOR(8,10);
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(50,11);
+bird_dead <= '1' when ('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479,10) - size) else '0';
 
 bird_on_signal <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
 					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) and (state = "01" or state = "10"))  else	-- y_pos - size <= pixel_row <= y_pos + size
@@ -40,8 +41,8 @@ begin
 				else 
 					ball_y_pos <= ball_y_pos - 70;
 				end if;
-			elsif (('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479,10) - size)) then	
-				null;
+			--elsif (('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479,10) - size)) then	
+				--null;
 			else 
 				ball_y_pos <= ball_y_pos +  gravity;
 		end if;
